8c80d4c76a9f7487d72247a203389f80398a1e8a
AmirH0 <amirhosseinbeyzaie@gmail.com>
2025-11-16 11:27:47 -0800

    tamrin -- add signals for create profile user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    GENDER_CHOICES = (
+        ("male", "Male"),
+        ("female", "Female"),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField(max_length=3, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    birth_data = models.DateField(blank=True, null=True)
+    gender = models.CharField(
+        max_length=10, choices=GENDER_CHOICES, blank=True, null=True
+    )
+    craeted_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}"
